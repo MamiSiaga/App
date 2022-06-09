fd4c2a7d3a2b117080c866df583e59f5cc75a643
rachmalifia <rachmalifia@gmail.com>
2022-06-09 20:59:05 +0700

    push notification & text recognition

---
+++ app/src/main/java/com/mamisiaga/PushNotificationService.kt
@@ -0,0 +1,56 @@
+package com.mamisiaga
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import com.mamisiaga.ui.MainActivity
+
+
+class PushNotificationService : FirebaseMessagingService() {
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onMessageReceived(message: RemoteMessage) {
+        if(message.notification != null){
+            generateNotification(message.notification!!.title!!, message.notification!!.body!! )
+        }
+        super.onMessageReceived(message)
+    }
+
+
+    private fun generateNotification(title: String, text: String) {
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT)
+        val builder: NotificationCompat.Builder = NotificationCompat.Builder(applicationContext, CHANNEL_ID)
+            .setContentTitle(title)
+            .setContentText(text)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setAutoCancel(true)
+            .setVibrate(longArrayOf(1000,1000,1000,1000))
+            .setOnlyAlertOnce(true)
+            .setContentIntent(pendingIntent)
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel = NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_HIGH)
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
+
+        notificationManager.notify(NOTIFICATION_ID, builder.build())
+    }
+
+    companion object {
+        const val NOTIFICATION_ID = 1
+        const val CHANNEL_ID = "HEADS_UP_NOTIFICATION"
+        const val CHANNEL_NAME = "Heads Up Notification"
+    }
+}
--- app/src/main/java/com/mamisiaga/tools/Utils.kt
+++ app/src/main/java/com/mamisiaga/tools/Utils.kt
@@ -1,8 +1,15 @@
 package com.mamisiaga.tools
 
 import android.app.Application
+import android.content.ContentResolver
+import android.content.Context
+import android.net.Uri
+import android.os.Environment
 import com.mamisiaga.R
 import java.io.File
+import java.io.FileOutputStream
+import java.io.InputStream
+import java.io.OutputStream
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -23,4 +30,24 @@ fun createFile(application: Application): File {
     ) mediaDir else application.filesDir
 
     return File(outputDirectory, "$timeStamp.jpg")
+}
+
+fun createTempFile(context: Context): File {
+    val storageDir: File? = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+    return File.createTempFile(timeStamp, ".jpg", storageDir)
+}
+
+fun uriToFile(selectedImg: Uri, context: Context): File {
+    val contentResolver: ContentResolver = context.contentResolver
+    val myFile = createTempFile(context)
+
+    val inputStream = contentResolver.openInputStream(selectedImg) as InputStream
+    val outputStream: OutputStream = FileOutputStream(myFile)
+    val buf = ByteArray(1024)
+    var len: Int
+    while (inputStream.read(buf).also { len = it } > 0) outputStream.write(buf, 0, len)
+    outputStream.close()
+    inputStream.close()
+
+    return myFile
 }
